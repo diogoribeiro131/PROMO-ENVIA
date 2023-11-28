8d59a6d03d7e6f46eb8abfb7ea91326a192205ba
Prismafive <64790468+Prismafive@users.noreply.github.com>
2023-11-28 10:57:19 -0300

    loggin inserido para log

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY . .
 
 CMD python app.py
 
+RUN pip install --upgrade pip
 RUN pip install requests
 RUN pip install bs4
 RUN pip install telebot
--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 import requests
 import bs4
 import telebot
+import logging
 
 # Configurações do Telegram
 bot = telebot.TeleBot("6338577772:AAEuZ-2gXuJVbmABSqXqcKYfcjSfO_xOt1g")
 
+# Configurações de log
+logging.basicConfig(filename='script_log.log', level=logging.INFO)
+
 # Lista de sites a serem monitorados
-sites = ["https://www.magazineluiza.com.br/", "https://www.amazon.com.br/", "https://www.americanas.com.br/", "https://www.americanas.com.br/", "https://www.mercadolivre.com.br/", "https://shopee.com.br/", "https://pt.aliexpress.com/", "https://www.netshoes.com.br/", "https://br.shein.com/"]
+sites = [
+    "https://www.magazineluiza.com.br/",
+    "https://www.amazon.com.br/",
+    "https://www.americanas.com.br/",
+    "https://www.americanas.com.br/",
+    "https://www.mercadolivre.com.br/",
+    "https://shopee.com.br/",
+    "https://pt.aliexpress.com/",
+    "https://www.netshoes.com.br/",
+    "https://br.shein.com/"
+]
 
 # Função para extrair o link da promoção
 def get_link_promocao(site):
     response = requests.get(site)
     if response.status_code != 200:
+        logging.warning(f"Falha ao acessar {site}. Código de status: {response.status_code}")
         return None
 
     soup = bs4.BeautifulSoup(response.content, "html.parser")
@@ -30,6 +45,7 @@ def get_link_promocao(site):
 # Função para enviar a mensagem via Telegram
 def enviar_mensagem(link):
     bot.send_message("+WPIVJvfHkJQ0YTRh", f"Nova promoção: {link}")
+    logging.info(f"Mensagem enviada: Nova promoção: {link}")
 
 # Loop principal
 for site in sites:
